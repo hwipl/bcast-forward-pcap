334fbbd36e1de1a3cf3e091f12472f40cd065b77
hwipl <33433250+hwipl@users.noreply.github.com>
2020-06-24 17:26:00 +0200

    add initial broadcast packet forwarding
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -1,5 +1,108 @@
 package cmd
 
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	gopktPcap "github.com/google/gopacket/pcap"
+	"github.com/hwipl/packet-go/pkg/pcap"
+)
+
+type handler struct {
+	outHandle *gopktPcap.Handle
+}
+
+func (h *handler) HandlePacket(packet gopacket.Packet) {
+	// get first udp layer
+	udpLayer := packet.Layer(layers.LayerTypeUDP)
+	if udpLayer == nil {
+		return
+	}
+	udp, _ := udpLayer.(*layers.UDP)
+	if udp.DstPort != 6112 {
+		return
+	}
+
+	// get first ip layer and modify destination IP
+	ipLayer := packet.Layer(layers.LayerTypeIPv4)
+	if ipLayer == nil {
+		return // panic?
+	}
+	ip, _ := ipLayer.(*layers.IPv4)
+	ip.DstIP = net.IPv4(192, 168, 1, 1)
+
+	// serialize layers
+	opts := gopacket.SerializeOptions{
+		FixLengths:       false,
+		ComputeChecksums: true,
+	}
+
+	// serialize modified ip layer
+	ipBuf := gopacket.NewSerializeBuffer()
+	err := ip.SerializeTo(ipBuf, opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// serialize udp layer to recalculate checksum
+	udpBuf := gopacket.NewSerializeBuffer()
+	udp.SetNetworkLayerForChecksum(ip)
+	err = udp.SerializeTo(udpBuf, opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// write all layers to buffer
+	var buf bytes.Buffer
+	l := packet.Layers()
+	for _, layer := range l {
+		fmt.Println(layer.LayerType())
+		switch layer.LayerType() {
+		case layers.LayerTypeIPv4:
+			buf.Write(ipBuf.Bytes())
+		case layers.LayerTypeUDP:
+			buf.Write(udpBuf.Bytes())
+		default:
+			buf.Write(layer.LayerContents())
+		}
+	}
+	fmt.Println(buf.Bytes())
+	h.outHandle.WritePacketData(buf.Bytes())
+}
+
+// listen captures packets on the network interface and parses them
+func listen() {
+	// create handler
+	var handler handler
+
+	// configure pcap
+	pcapFile := ""
+	pcapDevice := "wg0"
+	pcapFilter := "ip dst host 255.255.255.255"
+	pcapSnaplen := 2048
+
+	// create listener
+	listener := pcap.Listener{
+		PacketHandler: &handler,
+		File:          pcapFile,
+		Device:        pcapDevice,
+		Filter:        pcapFilter,
+		Snaplen:       pcapSnaplen,
+	}
+
+	// prepare listener and store pcap handle in handler
+	listener.Prepare()
+	handler.outHandle = listener.PcapHandle
+
+	// enter listener loop
+	listener.Loop()
+}
+
 // Run is the main entry point
 func Run() {
+	listen()
 }

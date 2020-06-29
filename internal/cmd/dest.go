0ac40051053f6dbe754797092d9a39cb765affa2
hwipl <33433250+hwipl@users.noreply.github.com>
2020-06-29 15:49:04 +0200

    add device to type dest
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/dest.go
+++ internal/cmd/dest.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"log"
 	"net"
+
+	"github.com/google/gopacket/pcap"
 )
 
 // dest stores information about a forwarding destination
 type dest struct {
+	dev   string
 	ip    net.IP
 	srcIP net.IP
 }
@@ -27,6 +30,27 @@ func (d *dest) getSourceIP() {
 	d.srcIP = laddr.IP
 }
 
+// getDevice gets the device used for the forwarding destination
+func (d *dest) getDevice() {
+	// get all network devices
+	devs, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// find the name of the device that uses the source IP address
+	for _, dev := range devs {
+		for _, addr := range dev.Addresses {
+			if addr.IP.Equal(d.srcIP) {
+				d.dev = dev.Name
+				return
+			}
+		}
+	}
+
+	log.Fatalf("did not find device for destination %s", d.ip)
+}
+
 // newDest creates and returns a new dest
 func newDest(addr string) *dest {
 	var dest dest
@@ -41,6 +65,9 @@ func newDest(addr string) *dest {
 
 	// get source IP
 	dest.getSourceIP()
+
+	// get device for source IP
+	dest.getDevice()
 
 	return &dest
 }
